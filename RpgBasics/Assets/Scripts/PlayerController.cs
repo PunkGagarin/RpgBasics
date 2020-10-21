39bc2c88e2f40d0914cc22082e50fef76395ead9
punkgagarin <mr.Tarasyuta@mail.ru>
2020-10-21 10:34:57 +0300

    base of movement and camera

---
+++ RpgBasics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+    public LayerMask movementMask;
+    PlayerMotor playerMotor;
+    void Start() {
+        cam = Camera.main;
+        playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask)) {
+                //Move to what we hit
+                playerMotor.MoveToPoint(hit.point);
+
+                //Stop focusing any objects (?)
+            }
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100)) {
+                //Check if interactable object
+                //Focus ont it if it is
+            }
+        }
+    }
+}
