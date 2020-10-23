35100db8ce64444b5ae1fcab168f66f36a53a438
punkgagarin <mr.Tarasyuta@mail.ru>
2020-10-23 08:49:13 +0300

    Character model

---
+++ RpgBasics/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomotionAnimationSmoothTime = .1f;
+
+    NavMeshAgent navMeshAgent;
+    Animator animator;
+
+    void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update() {
+        float speedPercent = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
