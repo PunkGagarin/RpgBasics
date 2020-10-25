7b476bc34a26c945cdaf707c289ad8389714344c
punkgagarin <mr.Tarasyuta@mail.ru>
2020-10-25 11:25:10 +0300

    base of stats

---
+++ RpgBasics/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+    void Start() {
+        EquipmentManager.GetInstance.onEquipmentChanged += OnEquipmentChange;
+    }
+
+    private void OnEquipmentChange(Equipment newItem, Equipment oldItem) {
+        if (newItem != null) {
+            armor.AddModifier(newItem.armorModifier);
+            attack.AddModifier(newItem.attackModifier);
+        }
+        if (oldItem != null) {
+            armor.AddModifier(oldItem.armorModifier);
+            attack.AddModifier(oldItem.attackModifier);
+        }
+    }
+}
---
+++ RpgBasics/Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat {
+
+    [SerializeField]
+    int baseValue = 0;
+
+    private List<int> modifiers = new List<int>();
+
+    public int GetValue() {
+        int finalValue = baseValue;
+        modifiers.ForEach(modifier => finalValue += modifier);
+        return finalValue;
+    }
+
+    public void AddModifier(int modifier) {
+        if(modifier != 0) {
+            modifiers.Add(modifier);
+        }
+    }
+
+    public void RemoveModifier(int modifier) {
+        if(modifier != 0) {
+            modifiers.Remove(modifier);
+        }
+    }
+}
